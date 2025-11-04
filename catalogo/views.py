d74291db8701011491f725814de7e0701147bfe4
Emanuel Gomez Bolig <egomezbolig@gmail.com>
2025-11-04 16:22:13 -0300

    Add carrusel Home

--- catalogo/views.py
+++ catalogo/views.py
@@ -6,8 +6,16 @@ from .models import Pelicula
 # Create your views here.
 
 
-class HomePageView(TemplateView):
-    template_name = "home.html"
+class HomePageView(ListView):
+    model = Pelicula  # Le decimos que consulte el modelo Pelicula
+    template_name = "home.html"  # Sigue usando la misma plantilla
+    context_object_name = "peliculas_top"  # Le damos un nombre al contexto
+
+    def get_queryset(self):
+        # 2. Esta es la consulta clave:
+        # Ordena las películas por puntuación (de mayor a menor)
+        # y toma solo las 5 primeras.
+        return Pelicula.objects.order_by('-puntuacion')[:5]
 
 
 class PeliculaListView(ListView):
