c22a7ec6216365c9ec88de1eacc7bf8e1dbdee4c
Emanuel Gomez Bolig <egomezbolig@gmail.com>
2025-11-04 14:42:11 -0300

    FIX: Actualizo ALLOWED_HOSTS para Render

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -30,6 +30,11 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+# Obtenemos el hostname de Render (ej: proyecto-peliculas.onrender.com)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
